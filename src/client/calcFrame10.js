c40e9a265a3684e41054d31ac06b3629ce3f68d7
zeyzen <64972509+zeyzen@users.noreply.github.com>
2023-01-06 16:58:16 +0100

    correction : getter of object frame

--- src/client/calcFrame10.js
+++ src/client/calcFrame10.js
@@ -1,16 +1,16 @@
 function calcFrame10(frame){
     var result = 0;
-    result = frame.c1;
+    result = frame.getC1();
 
     if(result === 10){ //mean we get a trike on the first throw
-        result += frame.c2 + frame.c3;
+        result += frame.getC2() + frame.getC3();
         return result;
     }
     
-    result += frame.c2;
+    result += frame.getC2();
 
     if (result === 10){ //mean we get a spare 
-        result += frame.c3;
+        result += frame.getC3();
         return result;
     }
     else{ //we got a normal round with only 2 throw
