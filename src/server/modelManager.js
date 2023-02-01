236f3ec5bc80bd4f7ab60b538f635e41690e97b5
SHirles <solene.hirles@mindpulse.net>
2023-02-01 15:02:55 +0100

    function createGrid

--- src/server/modelManager.js
+++ src/server/modelManager.js
@@ -1,5 +1,13 @@
+const Grid = require("./models/grid");
+
+var g;
+
 function createGrid(json)
 {
-
+    g=new Grid(json.nbKeel,json.nbFrame);
+    for (let i = 0; i < json.players.length; i++) {
+        g.addPlayer(json.players[i]);
+        
+    }
 }
 
