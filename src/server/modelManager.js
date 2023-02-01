db140f2a0adbbb42f256acd2ba61b9bf086d6eae
SHirles <solene.hirles@mindpulse.net>
2023-02-01 16:27:06 +0100

    function update grid and getGrid

--- src/server/modelManager.js
+++ src/server/modelManager.js
@@ -1,13 +1,39 @@
 const Grid = require("./models/grid");
+const Frame = require('./frame');
 
-var g;
+
+var grid;
 
 function createGrid(json)
 {
-    g=new Grid(json.nbKeel,json.nbFrame);
+    grid=new Grid(json.nbKeel,json.nbFrame);
     for (let i = 0; i < json.players.length; i++) {
-        g.addPlayer(json.players[i]);
+        grid.addPlayer(json.players[i]);
         
     }
+}
+
+function updateGrid(namePlayer, frame, element, value)
+{
+  if(grid.players.get(namePlayer)!=undefined){
+    switch (element) {
+        case 1:
+            grid.players[namePlayer][frame-1].setC1(value);
+          break;
+        case 2:
+            grid.players[namePlayer][frame-1].setC2(value);
+            break;
+        case 3:
+            grid.players[namePlayer][frame-1].setC3(value);
+            break;
+      }
+      
+  }
+  grid.calculScoreTotal(namePlayer);
+}
+
+function getGrid()
+{
+    return JSON.stringify(grid);
 }
 
